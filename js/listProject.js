d3508aeec1d01d7ef951b9c1c50705b77aa4e576
dungbubu <nguyenmanhdung1996@gmail.com>
2022-05-12 16:54:21 +0700

    Update lai giao dien va them hinh anh thumbnail cho cac san pham tren trang Project

--- js/listProject.js
+++ js/listProject.js
@@ -6,188 +6,202 @@ let list_project = [
 
 
   {
-    id:    23,
-    class: "webLandingpage",
-    name:  "Manison",
-    type:  "Web theme",
-    url:   "./img/project/Manison.webp",
-  },
-
-  {
     id:    22,
-    class: "webLandingpage",
-    name:  "Phân bón Phượng Hoàng",
-    type:  "Website",
-    url:   "./img/project/PhanBonPhuongHoang.webp",
+    class:      "presentation",
+    name:       "ITE HCMC",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/ITEHCMC(thumbnail).webp",
+    url:        "./img/project/ITEHCMC.webp",
   },
 
   {
     id:    21,
-    class: "presentation",
-    name:  "Growell",
-    type:  "PowerPoint",
-    url:   "./img/project/Growell.webp",
+    class:      "presentation",
+    name:       "Netco Post",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/NetcoPost(thumbnail).webp",
+    url:        "./img/project/NetcoPost.webp",
   },
 
   {
     id:    20,
-    class: "presentation",
-    name:  "Caraot",
-    type:  "PowerPoint",
-    url:   "./img/project/Caraot.webp",
+    class:      "webLandingpage",
+    name:       "Techmaster IOS Class",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/TechmasterIOSClass(thumbnail).webp",
+    url:        "./img/project/TechmasterIOSClass.webp",
   },
 
   {
     id:    19,
-    class: "webLandingpage",
-    name:  "Portfolio Bùi Đức Hoà",
-    type:  "Landing Page",
-    url:   "./img/project/PortfolioBuiDucHoa.webp",
+    class:      "webLandingpage",
+    name:       "Manison",
+    type:       "Web theme",
+    thumbnail:  "./img/project/Manison(thumbnail).webp",
+    url:        "./img/project/Manison.webp",
   },
 
   {
     id:    18,
-    class: "webLandingpage",
-    name:  "Domain Space",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainSpace.webp",
+    class:      "webLandingpage",
+    name:       "Phân bón Phượng Hoàng",
+    type:       "Website",
+    thumbnail:  "./img/project/PhanBonPhuongHoang(thumbnail).webp",
+    url:        "./img/project/PhanBonPhuongHoang.webp",
   },
 
   {
     id:    17,
-    class: "presentation",
-    name:  "FTA E&C",
-    type:  "PowerPoint",
-    url:   "./img/project/FTA.webp",
+    class:      "presentation",
+    name:       "Growell",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/Growell(thumbnail).webp",
+    url:        "./img/project/Growell.webp",
   },
 
   {
     id:    16,
-    class: "motionGraphic",
-    name:  "Zozo Mobile",
-    type:  "App Explainer",
-    url:   "./img/project/Zozo_Mobile.webp",
+    class:      "presentation",
+    name:       "Caraot",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/Caraot(thumbnail).webp",
+    url:        "./img/project/Caraot.webp",
   },
 
   {
     id:    15,
-    class: "webLandingpage",
-    name:  "Zozo Mobile",
-    type:  "Home Page",
-    url:   "./img/project/ZozoMobile.webp",
+    class:      "webLandingpage",
+    name:       "Portfolio Bùi Đức Hoà",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/PortfolioBuiDucHoa(thumbnail).webp",
+    url:        "./img/project/PortfolioBuiDucHoa.webp",
   },
 
   {
     id:    14,
-    class: "webLandingpage",
-    name:  "Domain Fun",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainFun.webp",
+    class:      "webLandingpage",
+    name:       "Domain Space",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainSpace(thumbnail).webp",
+    url:        "./img/project/iNETDomainSpace.webp",
   },
 
   {
     id:    13,
-    class: "webLandingpage",
-    name:  "Domain Host",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainHost.webp",
+    class:      "presentation",
+    name:       "FTA E&C",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/FTA(thumbnail).webp",
+    url:        "./img/project/FTA.webp",
   },
 
   {
     id:    12,
-    class: "webLandingpage",
-    name:  "Domain Tech",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainTech.webp",
+    class:      "webLandingpage",
+    name:       "Zozo Mobile",
+    type:       "Home Page",
+    thumbnail:  "./img/project/ZozoMobile(thumbnail).webp",
+    url:        "./img/project/ZozoMobile.webp",
   },
 
   {
     id:    11,
-    class: "webLandingpage",
-    name:  "Domain Store",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainStore.webp",
+    class:      "webLandingpage",
+    name:       "Domain Fun",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainFun(thumbnail).webp",
+    url:        "./img/project/iNETDomainFun.webp",
   },
 
   {
     id:    10,
-    class: "webLandingpage",
-    name:  "Domain Xyz",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainXyz.webp",
+    class:      "webLandingpage",
+    name:       "Domain Host",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainHost(thumbnail).webp",
+    url:        "./img/project/iNETDomainHost.webp",
   },
 
   {
     id:    9,
-    class: "presentation",
-    name:  "FirstAlliances",
-    type:  "PowerPoint",
-    url:   "./img/project/FirstAlliances.webp",
+    class:      "webLandingpage",
+    name:       "Domain Tech",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainTech(thumbnail).webp",
+    url:        "./img/project/iNETDomainTech.webp",
   },
 
   {
     id:    8,
-    class: "motionGraphic",
-    name:  "iNET Affiliate",
-    type:  "Video Marketing",
-    url:   "./img/project/iNET_Affiliate.webp",
+    class:      "webLandingpage",
+    name:       "Domain Store",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainStore(thumbnail).webp",
+    url:        "./img/project/iNETDomainStore.webp",
   },
 
   {
     id:    7,
-    class: "webLandingpage",
-    name:  "Domain Website",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainWebsite.webp",
+    class:      "webLandingpage",
+    name:       "Domain Xyz",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainXyz(thumbnail).webp",
+    url:        "./img/project/iNETDomainXyz.webp",
   },
 
   {
     id:    6,
-    class: "webLandingpage",
-    name:  "Domain Press",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainPress.webp",
+    class:      "presentation",
+    name:       "FirstAlliances",
+    type:       "PowerPoint",
+    thumbnail:  "./img/project/FirstAlliances(thumbnail).webp",
+    url:        "./img/project/FirstAlliances.webp",
   },
 
   {
     id:    5,
-    class: "motionGraphic",
-    name:  "BePage",
-    type:  "App Explainer",
-    url:   "./img/project/Bepage.webp",
+    class:      "webLandingpage",
+    name:       "Domain Website",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainWebsite(thumbnail).webp",
+    url:        "./img/project/iNETDomainWebsite.webp",
   },
 
   {
     id:    4,
-    class: "webLandingpage",
-    name:  "Domain Site",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainSite.webp",
+    class:      "webLandingpage",
+    name:       "Domain Press",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainPress(thumbnail).webp",
+    url:        "./img/project/iNETDomainPress.webp",
   },
 
   {
     id:    3,
-    class: "webLandingpage",
-    name:  "Domain Online",
-    type:  "Landing Page",
-    url:   "./img/project/iNETDomainOnline.webp",
+    class:      "webLandingpage",
+    name:       "Domain Site",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainSite(thumbnail).webp",
+    url:        "./img/project/iNETDomainSite.webp",
   },
 
   {
     id:    2,
-    class: "webLandingpage",
-    name:  "Tết 2022",
-    type:  "Landing Page",
-    url:   "./img/project/TetLandingPage.webp",
+    class:      "webLandingpage",
+    name:       "Domain Online",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/iNETDomainOnline(thumbnail).webp",
+    url:        "./img/project/iNETDomainOnline.webp",
   },
 
   {
     id:    1,
-    class: "motionGraphic",
-    name:  "MSC Loyalty System",
-    type:  "App Explainer",
-    url:   "./img/project/KDTech_LoyaltySystem.webp",
-  }
+    class:      "webLandingpage",
+    name:       "Tết 2022",
+    type:       "Landing Page",
+    thumbnail:  "./img/project/TetLandingPage(thumbnail).webp",
+    url:        "./img/project/TetLandingPage.webp",
+  },
 ];
 
 
@@ -195,7 +209,8 @@ let list_project = [
 var html = '';
 list_project.forEach(function(item) {
   html += `<div class="grid-item `+item.class+`">
-            <img src="`+item.url+`" alt="`+item.name+` `+item.type+`">
+            <img class="thumbnail_img" src="`+item.thumbnail+`" alt="`+item.name+` `+item.type+`">
+            <img class="project_img"   src="`+item.url+`"       alt="`+item.name+` `+item.type+`">
             <div class="content">
               <h4 class="name">`+item.name+`</h4>
               <p class="type">`+item.type+`</p>
